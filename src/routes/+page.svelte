4feedfdc7bac0bacfe78d7bc4ec2feed1d9b2314
BarasaJnr <isaacbarasa714@gmail.com>
2024-07-25 14:03:29 +0300

    fix type Writable in +page.svelte and BulkImport functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
   import BulkImport from '$lib/components/BulkImport.svelte';
   import TutorGrading from '$lib/components/TutorGrading.svelte';
   import RealtimeReport from '$lib/components/RealtimeReport.svelte';
-  
+
   type Question = {
     id: number;
     text: string;
